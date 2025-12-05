609eace57271ae0092eea7ceb4724921f2566335
mauriciobtsso <mauriciobtsso@gmail.com>
2025-12-05 16:43:02 -0300

    Corrige typo 'conn_health_check' para 'conn_health_checks' no settings.py

--- fepi_site/settings.py
+++ fepi_site/settings.py
@@ -47,7 +47,7 @@ if IS_RAILWAY_PROD:
         'default': dj_database_url.config(
             default=os.environ.get('DATABASE_URL'),
             conn_max_age=600,
-            conn_health_check=True,
+            conn_health_checks=True, # <--- CORRIGIDO: Adicionado o 's'
         )
     }
 
