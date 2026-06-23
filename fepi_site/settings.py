c0a5dc89256db4b57a0f5b4ab5d6e75516ac483f
mauriciobtsso <mauriciobtsso@gmail.com>
2026-06-23 13:05:06 -0300

    rastreador do erro debug true

--- fepi_site/settings.py
+++ fepi_site/settings.py
@@ -51,7 +51,7 @@ IS_PRODUCTION = 'RAILWAY_ENVIRONMENT' in os.environ or 'RENDER' in os.environ
 
 if IS_PRODUCTION:
     # MODO PRODUÇÃO (ONLINE) - Ativa a página 404 elegante automaticamente
-    DEBUG = False
+    DEBUG = True
     
     ALLOWED_HOSTS = [
         'fepiaui.org.br', 
